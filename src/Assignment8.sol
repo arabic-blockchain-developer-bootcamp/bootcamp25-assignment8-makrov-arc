34f159fb9ca1c3411711faccf538e66c5a6d953f
makrov-arc <reda24847@gmail.com>
2025-04-01 15:50:36 +0200

    cooling

--- src/Assignment8.sol
+++ src/Assignment8.sol
@@ -6,17 +6,24 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // make this contract inherits from: ERC721URIStorage contract
-contract Assignment8 is ERC721URIStorage, Ownable {
+contract Assignment8 is ERC721URIStorage {
     // declare a private uint called _tokenIdCounter
     uint256 private _tokenIdCounter;
+    address owner;
 
     // pass name and symbol of the nft token collection
-    constructor() ERC721("Makrov", "MKV") {}
+    constructor() ERC721("Makrov", "MKV") {
+        owner = msg.sender;
+    }
 
     // make this function visibility: external
     // pass string json uri parameter
     // restrict this function to be called only by the owner
     // Hint: You can use OpenZeppelin Ownable contract imported above
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
+    }
 
     function mintNFT(string memory jasonURI) external onlyOwner {
         // call _mint to mint a new nft to the function caller
